d2944693c42f0efb65de820ed160212a82ebcdef
YanaKonovalova93 <mikobacteria@gmail.com>
2023-06-26 12:43:14 +0300

    finish task 2

---
+++ src/components/Contacts/ContactsForm.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Formik, Field } from 'formik';
+import { FormBox } from './ContactsForm.styled';
+
+const initialValues = {
+  name: '',
+  number: '',
+};
+
+export const ContactsForm = ({ handleSubmit }) => {
+  const onSubmit = (value, { resetForm }) => {
+    handleSubmit(value);
+
+    resetForm();
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <FormBox>
+        <label>
+          Name
+          <Field
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label>
+          Number
+          <Field
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </FormBox>
+    </Formik>
+  );
+};
+
+ContactsForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+};
